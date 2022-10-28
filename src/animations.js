7f7a7c119cd2db5c2f81d820fe470a805fb15809
Oak atsume <56260438+OakAtsume@users.noreply.github.com>
2022-10-27 18:41:54 -0600

    Delete src directory

--- src/animations.js
+++
@@ -1,2 +0,0 @@
-This website doesn't use JavaScript, also code is compressed to allow faster loading.
-	What hosting service is used? well none! This website is hosted using a custom built program called Ropebunny (yes i'm kinky like that), Ropebunny uses simple TCP sockets to send and manage data!
